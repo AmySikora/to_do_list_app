4e882c4f81e465df5df13b1d80f9962252f99db9
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-07-13 16:34:24 -0700

    Updated js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-scripts.js
+//to select a list item on an ordered list with 
+//HTML ID of <ol id="list></ol>  use:
+$('#list');
+//If it's a class (<ol class="list"></ol>) use:
+$('.list');
+//To assign the selected list to a variable use:
+let list = $('#list');
+//to append something to the list use:
+$('#list').append(<li>list somenthing</li>);
+//to use the variable "list" created earier write:
+list.append("<li>list something</li>");
+//to store an <li> element as well as using:
+let li = $('<li></li>');
+//to append the li vairiable to the list variable using:
+list.append(li);
+
