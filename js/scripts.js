c88a5159641f6fe3833b2e2808afc49c7c421cec
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-07-15 15:11:44 -0700

    UPdated to add a class

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@
 //HTML ID of <ol id="list></ol>  use:
 $('#list');
 //If it's a class (<ol class="list"></ol>) use:
-$('.list');
+//$('.list');
 //To assign the selected list to a variable use:
 let list = $('#list');
+//to select and item in a class use:
+//$('.container')
 //to append something to the list use:
 $('#list').append("<li>Strawberries</li>");
 //to use the variable "list" created earier write:
@@ -14,11 +16,11 @@ let li = $('<li></li>');
 //to append the li vairiable to the list variable using:
 list.append(li);
 //to transverse the DOM elelemts: 
-let input= $('input');
-input.parent(); //get parent element
-input.children(); //get all children
-input.closest('.container'); //go up hierarchy of parents until selector matches
-input.siblings(); //get all siblings
+//let input= $('input');
+//input.parent(); //get parent element
+//input.children(); //get all children
+//input.closest('.container'); //go up hierarchy of parents until selector matches
+//input.siblings(); //get all siblings
 //to get input values from the index page: 
 $('#input').val();
 //To store the value into a variable use:
