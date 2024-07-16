2e28ed6d8cecff5cd36909ae09c026ef9bb70a23
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-07-15 17:02:14 -0700

    comment out code not used of the to do list

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,7 @@ $('body').append(newElement);
 //To add a strike through to a list item 
 li.addClass("strike");
 //to get input values from the index page: 
-$('#input').val("Snacks");
+$('#input').val("Dog food");
 //To store the value into a variable use:
 let inputValue = $('#input').val();
 //To append the value to another element: 
@@ -34,64 +34,64 @@ li.on("click", function() {
   });
 
 // to use Ajax to fetch with jQuery asynchronous requests:
-$.ajax('https://yesno.wtf/api', { dtataType: 'json'}).then(function (responseJSON) {
-    console.log(responseJSON); //parsed JSOn response
-});
+//$.ajax('https://yesno.wtf/api', { dtataType: 'json'}).then(function (responseJSON) {
+    //console.log(responseJSON); //parsed JSOn response
+//});
 //catch handle to chain handle errors:
-$.ajax('[URL]', {
-    method: 'POST',
-    dataType: 'json',
-    timeout: 5000
-}).then(function (responseJSON) {
-    console.log(responseJSON);
-}).catch(function(err) {
-    console.log('Caught and error:" + err.statusText');
-});
+//$.ajax('[URL]', {
+  //  method: 'POST',
+   // dataType: 'json',
+   // timeout: 5000
+//}).then(function (responseJSON) {
+    //console.log(responseJSON);
+//}).catch(function(err) {
+    //console.log('Caught and error:" + err.statusText');
+//});
 
 //Event handling adding and removing
-$('input').on('click', function (event) {
+//$('input').on('click', function (event) {
     // Do something
-});
-$('input').off('click'); //remove all "click" listeners
+//});
+//$('input').off('click'); //remove all "click" listeners
 
 //To execute a function on an indivdual element:
-$('input').each(function (i) {
-    let inputValue = $(this).val();
-    let inputName = $(this).attr('name');
-    if(inputValue().length < 1){
-        console.log('Please fill out the ' + inputName + ' field' )
-    } 
-});
+//$('input').each(function (i) {
+   // let inputValue = $(this).val();
+   // let inputName = $(this).attr('name');
+   // if(inputValue().length < 1){
+   //   console.//log('Please fill out the ' + inputName + ' field')
+   // }
+ // });  
 
 //DOM manipulation chaining to add flashing colors:
-$('notification')
-.addClass('red') 
-.delay(300)
-.removeClass('red');
+//$('notification')
+//.addClass('red') 
+//.delay(300)
+//.removeClass('red');
 
 //to transverse the DOM elelemts: 
-let input= $('input');
-input.parent(); //get parent element
-input.children(); //get all children
-input.closest('.container'); //go up hierarchy of parents until selector matches
-input.siblings(); //get all siblings
+//let input= $('input');
+//input.parent(); //get parent element
+//input.children(); //get all children
+//input.closest('.container'); //go up hierarchy of parents until selector matches
+//input.siblings(); //get all siblings
 
 //To show or hide HTML elements:
-let inputs= $('input');
-inputs.show(); //make them visible
-inputs.hide(); //hide the inputs
+//let inputs= $('input');
+//inputs.show(); //make them visible
+//inputs.hide(); //hide the inputs
 
-inputs.fadeOut(); //fade the elements in and out and then hide them
-inputs.fadeIn(1000); //fade the elementand them show them
-inputs.fadeToggle(); //fade the elelemts in/out depending on their visibility
+//inputs.fadeOut(); //fade the elements in and out and then hide them
+//inputs.fadeIn(1000); //fade the elementand them show them
+//inputs.fadeToggle(); //fade the elelemts in/out depending on their visibility
 
-inputs.slideDown(); //slide out of view and hide elements
-inputs.slideUp(); //slide elelemtns into view and show
-inputs.slideToggle(); //slide elements in and out
+//inputs.slideDown(); //slide out of view and hide elements
+//inputs.slideUp(); //slide elelemtns into view and show
+//inputs.slideToggle(); //slide elements in and out
 
 //to create, remove and append empty elements:
-input.remove(); //delete the element
-$('body').append(input); //similar to appendChild
-$('body').empty(); // removes element children
+//input.remove(); //delete the element
+//$('body').append(input); //similar to appendChild
+//$('body').empty(); // removes element children
 
 
